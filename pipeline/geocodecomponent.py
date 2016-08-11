80c87b2779d0964d915fc33a0811250166e9181a
haleystorm <haleystorm@users.noreply.github.com>
2016-08-10 17:17:27 -0700

    Initial commit of geocode component

--- pipeline/geocodecomponent.py
+++ pipeline/geocodecomponent.py
@@ -1,3 +1,5 @@
+import logging
+import requests
 from basecomponent import BaseComponent
 
 
@@ -6,15 +8,37 @@ class geocoder(BaseComponent):
 
     def initialize(self, parameters):
         self.dummydata = geocodes = [
-            {'name': 'Lima', 'lat': 32.345, 'lon': 45.32, 'adm1' : 'Peru', 'adm2' : 'AN'},
-            {'name': 'Stuebensville', 'lat': 32.345, 'lon': 45.32, 'adm1': 'USA', 'adm2': 'OH'}
+            {'name': 'Lima', 'lat': 32.345, 'lon': 45.32, 'adm1' : 'PE15', 'adm2' : None},
+            {'name': 'Stuebenville', 'lat': 32.345, 'lon': 45.32, 'adm1': 'US21', 'adm2': 'US21.0007'}
         ]
+        self.logger = logging.getLogger('scraper_log')
 
+    def geoparse(self, content):
+        try:
+            url = 'http://localhost:8182/opensextant/extract/geo/json'
+            files = {'infile': ('content.txt', content)}
+            response = requests.post(url, files=files)
+            return response.json()
+        except Exception, e:
+            print 'There was an error. Check the log file for more information.'
+            self.logger.warning('Problem requesting url: {}. {}'.format(url, e))
 
     def process(self, articles):
 
         for article in articles:
-            article['geocodes'] = self.dummydata
+            article['geocode'] = []
+            geocode_doc = self.geoparse(article['content'])
 
+            for anno in geocode_doc['annoList']:
+                geocode = { 'source': 'opensextant' }
+                geocode['matchText'] = anno['matchText']
+                geocode['name'] = anno['features']['place']['placeName']
+                geocode['countryCode'] = anno['features']['place']['countryCode']
+                geocode['lat'] = anno['features']['place']['latitude']
+                geocode['lon'] = anno['features']['place']['latitude']
+                geocode['adm1'] = anno['features']['place']['admin1']
+                geocode['adm2'] = anno['features']['place']['admin2']
+                article['geocode'].append(geocode)
+        
         return articles
 
